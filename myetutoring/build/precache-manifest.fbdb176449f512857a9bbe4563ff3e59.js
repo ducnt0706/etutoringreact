e725f805abc2e0b7da67e4194718fe3d2eadf229
Nguyễn Trung Đức <51743057+ducnt0706@users.noreply.github.com>
2020-05-02 11:42:44 +0700

    change project

---
+++ myetutoring/build/precache-manifest.fbdb176449f512857a9bbe4563ff3e59.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1b85ed387ca22cb477f296a3db2335",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cde679802d561e109ac7",
+    "url": "/static/css/2.2d42bffe.chunk.css"
+  },
+  {
+    "revision": "90e319c86bbaeb71ffb8",
+    "url": "/static/css/main.f2c2499b.chunk.css"
+  },
+  {
+    "revision": "cde679802d561e109ac7",
+    "url": "/static/js/2.2cb9af8f.chunk.js"
+  },
+  {
+    "revision": "271acf4d663e6edc2b1964edfcd913f8",
+    "url": "/static/js/2.2cb9af8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90e319c86bbaeb71ffb8",
+    "url": "/static/js/main.582d64f0.chunk.js"
+  },
+  {
+    "revision": "b906c161857354042e73",
+    "url": "/static/js/runtime-main.766b1727.js"
+  },
+  {
+    "revision": "586a01b08697e5ac2cd5a7fe9b76d76e",
+    "url": "/static/media/dashboard.586a01b0.ttf"
+  },
+  {
+    "revision": "6e8fabecc24b1c5d028bd32dbee36755",
+    "url": "/static/media/dashboard.6e8fabec.woff"
+  },
+  {
+    "revision": "c4c2ced1c622f76f730b8829895cedf1",
+    "url": "/static/media/dashboard.c4c2ced1.svg"
+  },
+  {
+    "revision": "ca5d2da2f8279167572f7590a2575e45",
+    "url": "/static/media/dashboard.ca5d2da2.eot"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "445eb8fb1157d1617417a82efada84d9",
+    "url": "/static/media/hero-img.445eb8fb.png"
+  },
+  {
+    "revision": "1cde5c7c91475cbe1b0b007f9b10d077",
+    "url": "/static/media/student.1cde5c7c.jpg"
+  }
+]);
--- myetutoring/src/firebaseconfig.js
+++ myetutoring/src/firebaseconfig.js
@@ -5,15 +5,15 @@ import 'firebase/firebase-storage';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDo_jngNKiKJer6Vt02rS2hRbc-Afb0JOQ",
-    authDomain: "etutoring-dc0f0.firebaseapp.com",
-    databaseURL: "https://etutoring-dc0f0.firebaseio.com",
-    projectId: "etutoring-dc0f0",
-    storageBucket: "etutoring-dc0f0.appspot.com",
-    messagingSenderId: "863849899471",
-    appId: "1:863849899471:web:c4b140e39195701e091eba",
-    measurementId: "G-FWWDQKXTQD"
-  };
+  apiKey: "AIzaSyBHXS8JoCZL2VLiNclsi2dYiOcwsPcZbv4",
+  authDomain: "etutoring-version1.firebaseapp.com",
+  databaseURL: "https://etutoring-version1.firebaseio.com",
+  projectId: "etutoring-version1",
+  storageBucket: "etutoring-version1.appspot.com",
+  messagingSenderId: "664401817574",
+  appId: "1:664401817574:web:299cda2a7fcead9a447027",
+  measurementId: "G-TKDTQJJ9Y8"
+};
 
 firebase.initializeApp(firebaseConfig);
 
